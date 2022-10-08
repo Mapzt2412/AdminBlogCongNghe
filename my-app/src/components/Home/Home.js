f2bbcd9df7f92c0c3706b03178971d2819cad65e
Quang Canh <lequangcanh2810@gmail.com>
2022-10-08 18:48:40 +0700

    frame-admin-CANH

---
+++ my-app/src/components/Home/Home.js
@@ -0,0 +1,9 @@
+import MainLayout from "../MainLayout/MainLayout.js"
+
+const Home = () => {
+    return <>
+        <MainLayout leftMenu={'leftMenu'} contentHeader={'contentHeader'} content={'content'} />
+    </>
+} 
+
+export default (Home)
